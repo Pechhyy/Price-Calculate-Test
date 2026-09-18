2a549b15f12aef72b42cde416c0aeff07f007594
pechhyy <pechna212@gmail.com>
2026-09-18 21:03:12 +0700

    fix(add-gitignore): fixed: handle add gitignore for pushing code

---
+++ items.ts
@@ -0,0 +1,19 @@
+export enum MenuItem {
+  Red = "Red",
+  Green = "Green",
+  Blue = "Blue",
+  Yellow = "Yellow",
+  Pink = "Pink",
+  Purple = "Purple",
+  Orange = "Orange",
+}
+
+export type Order = {
+  Red?: number;
+  Green?: number;
+  Blue?: number;
+  Yellow?: number;
+  Pink?: number;
+  Purple?: number;
+  Orange?: number;
+};
